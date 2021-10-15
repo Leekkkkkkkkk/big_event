c4cb2d987b4d17f31df3ee91bc124cba22f15639
branches <1425736923@qq.com>
2021-10-15 15:34:36 +0800

    注册功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,12 +8,27 @@ $(function () {
     $(".login-box").show();
     $(".reg-box").hide();
   });
-});
-layui.form.verify({
-  pwd: [/^\S{6,12}$/, "密码长度是6-12位非空格组成"],
-  repwd: function (value, item) {
-    if (value != $(item).parent().prev().find("input").val()) {
-      return "两次密码输入不一致";
-    }
-  },
+
+  //表单验证
+  layui.form.verify({
+    pwd: [/^\S{6,12}$/, "密码长度是6-12位非空格组成"],
+    repwd: function (value, item) {
+      if (value != $(item).parent().prev().find("input").val()) {
+        return "两次密码输入不一致";
+      }
+    },
+  });
+
+  //登录注册
+  $("#form_reg").submit(function (e) {
+    e.preventDefault(); //阻止默认行为
+    $.post('http://api-breakingnews-web.itheima.net/api/reguser', {
+      username: $("#reg_username").val(),
+      password: $("#reg_password").val(),
+    },function(res){
+      if(res.status != 0) return layui.layer.msg(res.message);
+      layui.layer.msg('注册成功')
+      $('#link_login').click()
+    });
+  });
 });
